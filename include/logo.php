be95bb3036dfcacab63d83a8814ead71f74f0907
neeil1990 <neeil@mail.ru>
2018-05-18 11:37:21 +0300

    no message

--- include/logo.php
+++ include/logo.php
@@ -1,3 +1,3 @@
-<a href="<?= SITE_DIR ?>">
-    <img src="<?= SITE_DIR ?>images/intec.png">
-</a>
+<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH?>/incut/incut.js"></script>
+<link rel="stylesheet" href="<?=SITE_TEMPLATE_PATH?>/incut/incut.css">
+<a class="prime-incut white colour marketing-only"></a>
