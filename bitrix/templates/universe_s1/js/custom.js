94b3812416707b295c844a0577c2899238597ff1
neeil1990 <neeil@mail.ru>
2018-06-09 15:00:24 +0300

    no message

--- bitrix/templates/universe_s1/js/custom.js
+++ bitrix/templates/universe_s1/js/custom.js
@@ -6,3 +6,14 @@ $('.readmore').readmore({
     moreLink: '<div style="margin-top: 20px" class="intec-about-company-more"><a href="" class="intec-button intec-button-md intec-button-cl-common intec-button-transparent">Подробнее</a></div>',
     lessLink: '<div class="intec-about-company-more"><a href="" class="intec-button intec-button-md intec-button-cl-common intec-button-transparent">Скрыть</a></div>',
 });
+
+$('.intec-min-button-compare.add').click(function(){
+    $(this).closest('.catalog-section-element').find('.element-img img').effect("transfer", { to: $('.flying-basket_buttons_wrap a[href="\/catalog\/compare.php"]'), className: "transfer_class" }, 800);
+    var img = $('<img />', {
+        src: $(this).closest('.catalog-section-element').find('.element-img img').attr('src')
+    });
+    $('.transfer_class').html(img);
+});
+
+
+
