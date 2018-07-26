3503798bdd5423c1338b78605293eb4990b62709
neeil1990 <neeil@mail.ru>
2018-07-26 16:19:19 +0300

    no message

---
+++ bitrix/modules/prime.smartbanners/lang/ru/classes/Nb_class.php
@@ -0,0 +1,15 @@
+<?
+$MESS["PUBLIC_BANNER_ICON"] = "Редактирование баннера";
+$MESS["BANNER_BAD_NAME"] = "Не указано имя баннера";
+$MESS["BANNER_BAD_BANTYPE"] = "Не указана рекламная площадка";
+$MESS["BANNER_BAD_SHOW_FROM"] = "Неправильно указан интервал показа";
+$MESS["BANNER_BAD_SHOW_TO"] = "Неправильно указан интервал показа";
+$MESS["BANTYPE_BAD_NAME"] = "Не указано название площадки";
+$MESS["BANTYPE_BAD_CODE"] = "Не указан код площадки";
+$MESS["BANTYPE_BAD_CODE_SYNT"] = "Код должен состоять из латинских букв, цифр и символов '-' и '_', причем начинаться обязательно с буквы";
+$MESS["BANTYPE_CODE_EXISTS"] = "Площадка с таким кодом уже существует";
+$MESS["EDIT_BANNER"] = "Редактировать баннер #";
+$MESS["ADD_BANNER"] = "Добавить баннер";
+$MESS["BANNER_BAD_IMAGE"] = "Не удалось сохранить файл";
+$MESS["BANNER_BAD_SITE"] = "Не выбран сайт";
+?>
