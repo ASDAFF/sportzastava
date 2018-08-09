c3137dc2e6c9c52dc40e71854f11b91813e9f78f
neeil1990 <neeil@mail.ru>
2018-08-09 18:16:19 +0300

    no message

--- bitrix/templates/universe_s1/js/custom.js
+++ bitrix/templates/universe_s1/js/custom.js
@@ -7,8 +7,26 @@ $('.readmore').readmore({
     lessLink: '<div class="intec-about-company-more"><a href="" class="intec-button intec-button-md intec-button-cl-common intec-button-transparent">Скрыть</a></div>',
 });
 
+
+
 $('.intec-min-button-compare.add').click(function(){
     $(this).closest('.catalog-section-element').find('.element-img img').effect("transfer", { to: $('.widget-flying-basket-footer a[href="\/catalog\/compare.php"]'), className: "transfer_class" }, 800);
+    var img = $('<img />', {
+        src: $(this).closest('.catalog-section-element').find('.element-img img').attr('src')
+    });
+    $('.transfer_class').html(img);
+});
+
+$('.intec-min-button-like.add').click(function(){
+    $(this).closest('.catalog-section-element').find('.element-img img').effect("transfer", { to: $('.widget-flying-basket-footer a[href="\/personal\/basket\/\?delay\=y"]'), className: "transfer_class" }, 800);
+    var img = $('<img />', {
+        src: $(this).closest('.catalog-section-element').find('.element-img img').attr('src')
+    });
+    $('.transfer_class').html(img);
+});
+
+$('.element-buys.add').click(function(){
+    $(this).closest('.catalog-section-element').find('.element-img img').effect("transfer", { to: $('.widget-flying-basket-footer a[href="\/personal\/basket\/"]'), className: "transfer_class" }, 800);
     var img = $('<img />', {
         src: $(this).closest('.catalog-section-element').find('.element-img img').attr('src')
     });
